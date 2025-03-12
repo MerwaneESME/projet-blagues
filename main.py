c2211eaac3157e5ac6f1a7cd2fda958d5d7830a1
Merwane <merwane.tounsi@esme.fr>
2025-03-12 17:43:05 +0100

    correction main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pyjokes
 
-def tell_joke(lang="en"):
-    print(pyjokes.get_joke(language=lang))
+def tell_joke(lang="en", category="all"):
+    print(pyjokes.get_joke(language=lang, category=category))
 
 if __name__ == "__main__":
-    lang = input("Choisissez une lange (en, de, es, it, gl):")
-    joke_type = input("Choisissez une catégorie de blague (neutral, chuck, all):")
+    lang = input("Choisissez une langue (en, de, es, it, gl): ")
+    joke_type = input("Choisissez une catégorie de blague (neutral, chuck, all): ")
     tell_joke(lang, joke_type)
